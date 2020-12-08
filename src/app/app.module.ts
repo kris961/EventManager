6e396ebe941908a1fd61feaa7712ed9ff34e4888
2345345346kbv <kristian.mihnev@abv.bg>
2020-12-08 11:44:35 +0200

    added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AngularFirestore, AngularFirestoreModule} from '@angular/fire/firestore'
 import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
+import { CoreModule } from './core/core.module';
+import { HeaderComponent } from './core/header/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CoreModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
+    AngularFirestoreModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    HeaderComponent
+  ]
 })
 export class AppModule { }
