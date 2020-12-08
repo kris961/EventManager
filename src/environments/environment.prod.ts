0ab884bee44dee92796c0ea7cb76b447275de5ac
2345345346kbv <kristian.mihnev@abv.bg>
2020-12-08 09:45:39 +0200

    added firebase cli

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAa4LYcji9AJ9gZ8daysABu9k3qE8etXTI",
+    authDomain: "eventmanager-c9876.firebaseapp.com",
+    projectId: "eventmanager-c9876",
+    storageBucket: "eventmanager-c9876.appspot.com",
+    messagingSenderId: "531505761513",
+    appId: "1:531505761513:web:0ef12a6217a29094457478",
+    measurementId: "G-69EY87M4QD"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAa4LYcji9AJ9gZ8daysABu9k3qE8etXTI",
+    authDomain: "eventmanager-c9876.firebaseapp.com",
+    projectId: "eventmanager-c9876",
+    storageBucket: "eventmanager-c9876.appspot.com",
+    messagingSenderId: "531505761513",
+    appId: "1:531505761513:web:0ef12a6217a29094457478",
+    measurementId: "G-69EY87M4QD"
+  }
 };
 
 /*
